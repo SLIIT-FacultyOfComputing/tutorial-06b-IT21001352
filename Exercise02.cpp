fa866179f801e680483d7773fc43f7f9c639ee0b
IT21001352 <87793405+IT21001352@users.noreply.github.com>
2021-08-18 12:57:51 +0000

    tutorial-06

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main() {
   
    // 1. Create a Box type object called box1
+
+   Box box1;
   
   
    // ======= DO NOT CHANGE THE INPUT ===========
-   int height, length, width;
+   int height,length, width;
    cout << "Enter the Height of the Box : ";
    cin >> height;
    cout << "Enter the Length of the Box : ";
@@ -18,6 +20,9 @@ int main() {
    // ===========================================
   
    // 2. Use setters assign height, length, width throw
+    box1.setLength(length);
+    box1.setWidth(width);
+    box1.setHeight(height);
   
    // === DO NOT CHANGE THE OUTPUT =============
    cout << "Box Height " << box1.getHeight() << endl;
